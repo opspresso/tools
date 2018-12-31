e0cd8032c8aa99833ee1a7f887940ae0dc1f6833
nalbam <me@nalbam.com>
2019-01-01 04:35:55 +0900

    terraform

---
+++ bin/terraform.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+NAME=${1:-terraform}
+
+BUCKET="repo.opspresso.com"
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+_prepare() {
+    CONFIG=~/.config/opspresso/latest
+    mkdir -p ${CONFIG} && touch ${CONFIG}/${NAME}
+
+    TMP=/tmp/opspresso/tools
+    mkdir -p ${TMP}
+
+    _brew
+}
+
+_brew() {
+    if [ "${OS_NAME}" == "darwin" ]; then
+        command -v brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+}
+
+_compare() {
+    NOW=$(cat ${CONFIG}/${NAME} | xargs)
+    NEW=$(curl -sL ${BUCKET}/latest/${NAME} | xargs)
+
+    if [ "${NEW}" != "" ] && [ "${NEW}" != "${NOW}" ]; then
+        printf '%-10s %-10s\n' "${NOW}" "${NEW}"
+
+        VERSION="${NEW}"
+
+        printf "${VERSION}" > ${CONFIG}/${NAME}
+    fi
+}
+
+echo "================================================================================"
+echo "install ${NAME}..."
+
+_prepare
+_compare
+
+if [ "${VERSION}" != "" ]; then
+    if [ "${OS_NAME}" == "darwin" ]; then
+        command -v terraform > /dev/null || brew install terraform
+    else
+        URL="https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_${OS_NAME}_amd64.zip"
+        curl -LO ${URL}
+        unzip terraform_${VERSION}_${OS_NAME}_amd64.zip && rm -rf terraform_${VERSION}_${OS_NAME}_amd64.zip
+        sudo mv ${NAME} /usr/local/bin/${NAME}
+    fi
+fi
+
+terraform version | xargs | awk '{print $2}'
