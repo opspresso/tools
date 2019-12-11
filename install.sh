58d108a7f7518e1dc7d4d2c1da8875be3d42985d
nalbam <me@nalbam.com>
2019-12-11 13:03:01 +0900

    add semver

--- install.sh
+++ install.sh
@@ -32,5 +32,8 @@ curl -sL ${REPO}/nodejs | bash -s "13"
 # aws-iam-authenticator
 curl -sL ${REPO}/aws-iam-authenticator | bash -s "v0.3.0"
 
+# semver
+curl -sL ${REPO}/semver | bash -s "3.0.0"
+
 # clean
 curl -sL ${REPO}/clean | bash
