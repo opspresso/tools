b7d549bcd3c36fd9fec445e9e6f46c37ce64e1cb
nalbam <me@nalbam.com>
2020-05-27 10:52:25 +0900

    add istioctl

---
+++ istioctl.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+NAME="istioctl"
+
+VERSION=${1}
+
+OS_NAME="$(uname | awk '{print tolower($0)}')"
+
+_prepare() {
+    CONFIG=~/.config/opspresso/latest
+    mkdir -p ${CONFIG} && touch ${CONFIG}/${NAME}
+
+    TMP=/tmp/opspresso/tools
+    mkdir -p ${TMP}
+
+    _brew
+}
+
+_brew() {
+    if [ "${OS_NAME}" == "darwin" ]; then
+        command -v brew > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+}
+
+_compare() {
+    touch ${CONFIG}/${NAME}
+    NOW=$(cat ${CONFIG}/${NAME} | xargs)
+
+    if [ "${VERSION}" != "" ]; then
+        NEW="${VERSION}"
+    else
+        NEW=$(curl -sL opspresso.github.io/${NAME}/LATEST | xargs)
+    fi
+
+    if [ "${NEW}" != "" ] && [ "${NEW}" != "${NOW}" ]; then
+        echo "${NOW:-new} >> ${NEW}"
+
+        VERSION="${NEW}"
+    else
+        VERSION=""
+    fi
+}
+
+echo "================================================================================"
+echo "install ${NAME} ${VERSION}..."
+
+_prepare
+_compare
+
+if [ "${VERSION}" != "" ]; then
+    if [ "${OS_NAME}" == "darwin" ]; then
+        command -v istioctl > /dev/null || brew install istioctl
+    else
+        URL="https://github.com/istio/istio/releases/download/${VERSION}/istioctl-${VERSION}-${OS_NAME}.tar.gz"
+        curl -L ${URL} | tar xz
+        sudo mv ${NAME} /usr/local/bin/${NAME}
+    fi
+
+    printf "${VERSION}" > ${CONFIG}/${NAME}
+fi
+
+istioctl version
